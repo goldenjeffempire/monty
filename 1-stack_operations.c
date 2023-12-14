28066152f296e35c1d8c3a31fd6cccb7005dce55
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-12-13 21:48:02 -0800

    Stack operations

--- 1-stack_operations.c
+++ 1-stack_operations.c
@@ -80,7 +80,7 @@ void display_stack(stack_t **stack, unsigned int line_number)
 void display_top(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
-		more_err(6, line_number);
+		handle_additional_error(6, line_number);
 	printf("%d\n", (*stack)->n);
 }
 /**
@@ -94,7 +94,7 @@ void remove_top(stack_t **stack, unsigned int line_number)
 	stack_t *tmp;
 
 	if (stack == NULL || *stack == NULL)
-		more_err(7, line_number);
+		handle_additional_error(7, line_number);
 
 	tmp = *stack;
 	*stack = tmp->next;
